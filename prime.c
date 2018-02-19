86bc2ee4d5b65853bbf7ccfea4182ab82dbe108f
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-19 16:43:37 +0530

    prime.c
    good

---
+++ prime.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int high;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &low, &high);
+    printf("Enter the prime number are:",high);
+    {
+    high==1;
+    break;
+    }
+  return;
+  }
+       
